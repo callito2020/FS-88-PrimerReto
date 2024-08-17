27e421d014ae0485bfe452b904d1c2d3b2b8e66c
unknown <claudechateau71@gmail.com>
2024-08-16 21:15:07 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Cuando el documento esté listo
+// (no necesariamente el contenido de las etiquetas)
+
+$(document).ready(function() {
+    // Ejecuta esto
+    console.time('jQuery')
+    $('form').on("submit", function() {
+      console.log('Formulario1 enviado 🕊️')
+    })
+    console.timeEnd('jQuery')
+  
+    $('#agrandarTexto').on("click", function() {
+      // elemento.css("propiedad", "valor")
+      $('#parrafoObjetivoAgrandar').css({
+        'color': 'red',
+        'fontSize': '25px'
+      })
+    })
+  
+    $('#targetThis').on('mouseenter', function() {
+      console.log(this)
+    })
+  })
+  
